435ae266378a17957ade6ff66d27c001e55a99b2
Tue Nguyen <ntue.ntshop@gmail.com>
2019-07-18 17:18:43 +0700

    change query in cart view

--- resources/views/layouts/alsolike.blade.php
+++ resources/views/layouts/alsolike.blade.php
@@ -4,9 +4,9 @@
         <div class="flex -mx-2 text-center pt-8" >
             @foreach ($mightAlsoLike as $product)
             <div class="w-1/4 border-2 border-gray-500 mx-2 bg-white w-64 h-64 pt-8">
-                <a href="{{ route('shop.show', ['product' => $product]) }}"><img class="mx-auto "
+                <a href="{{ route('shop.show', $product->slug) }}"><img class="mx-auto "
                         src="/img/products/{{ $product->slug }}.jpg" alt="product"></a>
-                <a href="{{ route('shop.show', ['product' => $product]) }}"
+                <a href="{{ route('shop.show', $product->slug) }}"
                     class="font-semibold">{{ $product->name }}</a>
                 <p>{{ $product->presentPrice() }}</p>
             </div>
